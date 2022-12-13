d6c99906c3b3e019178bf7d06e4594e059a47e69
Will Gyuha Yi <yiguha@gmail.com>
2022-12-13 17:08:16 +0900

    nginx js,css 파일 적용
    nginx js,css 파일 적용

--- NWAD/NWAD/settings.py
+++ NWAD/NWAD/settings.py
@@ -138,7 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_DOMAIN = ''
 
