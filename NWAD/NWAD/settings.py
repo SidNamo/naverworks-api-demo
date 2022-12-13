43e3d6ce9a61ba37da76919eb8802e6512b37398
Will Gyuha Yi <yiguha@gmail.com>
2022-12-13 15:16:30 +0900

    nginx 연동을 위한 셋팅
    nginx 연동을 위한 셋팅

--- NWAD/NWAD/settings.py
+++ NWAD/NWAD/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-m25nt4^f#4luyhdn^iz2sm^l=*tsau-rk)s3g9tr-)gijqsrth
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','3.39.227.8', 'didimapisimpler.com']
+ALLOWED_HOSTS = ['*.compute.amazonaws.com', '127.0.0.1','3.39.227.8', 'didimapisimpler.com']
 
 
 # Application definition
