ea32182245849262c4440fbfbd4dd5e24cab206d
kjh <haheehong3@naver.com>
2022-12-13 17:26:14 +0900

    BASE_DIR 경로 변경

--- NWAD/NWAD/settings.py
+++ NWAD/NWAD/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -138,8 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_DOMAIN = ''
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
