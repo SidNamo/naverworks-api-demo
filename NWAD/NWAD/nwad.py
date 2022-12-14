3c5434eaa44910eb4ad08b73f0e67479ecf55cbc
kjh <haheehong3@naver.com>
2022-12-14 17:03:30 +0900

    메세지 인코딩 수정

--- NWAD/NWAD/nwad.py
+++ NWAD/NWAD/nwad.py
@@ -581,7 +581,7 @@ def botResponse(request):
                                 channelId = ""
 
                             if channelId == "":
-                                sender["message"] = util.unicodeAddSlash(req["content"]["text"])
+                                sender["message"] = req["content"]["text"]
                                 util.insertLog(request, "받은 메시지: " + sender["message"])
                                 # 연결중 인지 체크
                                 scenConnData = scen_conn.objects.filter(reporter = sender["id"]).exclude(status = 9).first()
