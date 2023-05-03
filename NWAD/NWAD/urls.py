7e664a74f592a3444724115e644640e73d2e167b
Will Gyuha Yi <yiguha@gmail.com>
2023-05-03 17:39:58 +0900

    Update urls.py

--- NWAD/NWAD/urls.py
+++ NWAD/NWAD/urls.py
@@ -5,7 +5,7 @@ from . import nwad
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('robots.txt',  TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
+    path(r'^robots\.txt/$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),,
     
     # path('admin/', admin.site.urls),
     path('auth/', include('AUTH.urls')),
